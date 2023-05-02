2c57442504cdc263ff6e937580ff7d7c2116e63f
Geovana <geovana.170995@gmail.com>
2023-05-01 22:07:34 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignComponent } from './components/sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'logar',
+    component: LoginComponent
+  },
+  {
+    path: 'criar-conta',
+    component: SignComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'logar',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './components/login/login.module';
+import { SignModule } from './components/sign/sign.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    SignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
